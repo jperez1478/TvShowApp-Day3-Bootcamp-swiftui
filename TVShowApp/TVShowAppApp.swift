cc5aa4e1f4a4799e13ba5040a46b9cd30393fe8c
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-08 11:18:30 -0500

    Initial Commit

---
+++ TVShowApp/TVShowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVShowAppApp.swift
+//  TVShowApp
+//
+//  Created by Jessica Perez on 8/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TVShowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

3ca69219ede1a284d3dfe8bc154dea07194bdf57
jperez1478 <61983873+jperez1478@users.noreply.github.com>
2021-08-08 12:26:14 -0500

    -on appear to test data go back  because it ran to error

--- TVShowApp/Views/ShowsView.swift
+++ TVShowApp/Views/ShowsView.swift
@@ -7,12 +7,24 @@
 
 import SwiftUI
 
+
+
+//MARK: - Body
 struct ShowsView: View {
+    
+    //object observe
+   @ObservedObject var showViewModel = ShowViewModel()
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onAppear(perform: {
+                showViewModel.fetchTVShows()
+            })
     }
 }
+
+//MARK: - Preview
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
